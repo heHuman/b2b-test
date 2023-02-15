e50f22f2196e60a36a149c3cb182e051fc77f6a3
Nikita Kropin <nik.kropin@gmail.com>
2023-02-16 00:19:51 +0400

    Tests to prevent from creating sockets with empty configurations

--- src/app/components/socket-configurator/socket-configurator.component.spec.ts
+++ src/app/components/socket-configurator/socket-configurator.component.spec.ts
@@ -1,23 +1,61 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { SocketConfiguratorService } from 'src/app/services/socket-configurator.service';
 
 import { SocketConfiguratorComponent } from './socket-configurator.component';
 
-describe('SocketConfiguratorComponent', () => {
-  let component: SocketConfiguratorComponent;
-  let fixture: ComponentFixture<SocketConfiguratorComponent>;
+fdescribe('SocketConfiguratorComponent', () => {
+    let component: SocketConfiguratorComponent;
+    let fixture: ComponentFixture<SocketConfiguratorComponent>;
+    let configuratorServiceSpy: SocketConfiguratorService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SocketConfiguratorComponent ]
-    })
-    .compileComponents();
 
-    fixture = TestBed.createComponent(SocketConfiguratorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        configuratorServiceSpy = jasmine.createSpyObj(SocketConfiguratorService, ['setTimerValue', 'setArraySize']);
+        await TestBed.configureTestingModule({
+            declarations: [SocketConfiguratorComponent],
+            providers: [
+                { provide: SocketConfiguratorService, useValue: configuratorServiceSpy }
+            ]
+        })
+            .compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        TestBed.inject(SocketConfiguratorService);
+
+        fixture = TestBed.createComponent(SocketConfiguratorComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    describe('onTimerValueChanged called with a truthy parameter', () => {
+        it('should call SocketConfiguratorService.setTimerValue', () => {
+            component.onTimerValueChanged('araeg');
+            expect(configuratorServiceSpy.setTimerValue).toHaveBeenCalled();
+        });
+    });
+
+    describe('onTimerValueChanged called with a falsy parameter', () => {
+        it('should not call SocketConfiguratorService.setTimerValue', () => {
+            component.onTimerValueChanged('');
+            expect(configuratorServiceSpy.setTimerValue).not.toHaveBeenCalled();
+        });
+    });
+
+    describe('onArraySizeChanged called with a falsy parameter', () => {
+        it('should not call SocketConfiguratorService.setArraySize', () => {
+            component.onArraySizeChanged('');
+            expect(configuratorServiceSpy.setArraySize).not.toHaveBeenCalled();
+        });
+    });
+
+    describe('onArraySizeChanged called with a truthy parameter', () => {
+        it('should call SocketConfiguratorService.setArraySize', () => {
+            component.onArraySizeChanged('araeg');
+            expect(configuratorServiceSpy.setArraySize).toHaveBeenCalled();
+        });
+    });
 });
